710f9174d23a0254bd2ae3892eac5072ae7b4c8a
lemus777 <lemus777@gmail.com>
2024-08-22 18:49:06 +0200

    colors added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-  console.log(`Rest api funcionando en el puerto ${port}`)
+  console.log( colors.cyan.bold( `Rest api funcionando en el puerto ${port}` ))
 })
