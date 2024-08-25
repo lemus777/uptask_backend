5fd97b6b2a932cf3997b5cf413fe7ad128008e6d
lemus777 <lemus777@gmail.com>
2024-08-25 13:21:06 +0200

    endpoint for get a project by id created

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { body } from "express-validator"
+import { body, param } from "express-validator"
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middleware/validation";
 
@@ -16,5 +16,11 @@ router.post('/',
   ProjectController.createProject
 )
 router.get('/', ProjectController.getAllProjects)
+router.get('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  ProjectController.getProjectById
+
+)
 
 export default router
