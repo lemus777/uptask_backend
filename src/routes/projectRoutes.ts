5bad34d96effde183a40e77b274f0dfa8fcfd2c0
lemus777 <lemus777@gmail.com>
2024-08-25 13:34:42 +0200

    endpoint for update a project created

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -20,7 +20,17 @@ router.get('/:id',
   param('id').isMongoId().withMessage('ID no válido'),
   handleInputErrors,
   ProjectController.getProjectById
-
+)
+router.put('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+  body('description')
+    .notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+  handleInputErrors,
+  ProjectController.updateProject
 )
 
 export default router
