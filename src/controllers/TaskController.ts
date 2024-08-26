378b043ac3c37dc9b0a3531c2f2d0a29cde09376
lemus777 <lemus777@gmail.com>
2024-08-26 18:07:02 +0200

    endpoint for update task created

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -41,4 +41,22 @@ export class TaskController {
       res.status(500).json({error: 'Hubo un error'})
     }
   }
+
+  static updateTask = async (req : Request, res : Response) => {
+    try {
+      const { taskId } = req.params
+      const task = await Task.findByIdAndUpdate(taskId, req.body)
+      if (!task) {
+        const error = new Error('Tarea no encontrada')
+        return res.status(404).json({error: error.message})
+      }
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error('Acción no válida')
+        return res.status(400).json({error: error.message})
+      }
+      res.send('Tarea actualizada correctamente')
+    } catch (error) {
+      res.status(500).json({error: 'Hubo un error'})
+    }
+  }
 }
