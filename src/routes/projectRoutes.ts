5ea0710546f84a58f3e06b8a18ac32bad747cddc
lemus777 <lemus777@gmail.com>
2024-08-26 17:38:17 +0200

    get task by ID and validation

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -60,4 +60,9 @@ router.get('/:projectId/tasks',
   TaskController.getProjectTasks
 )
 
+router.get('/:projectId/tasks/:taskId',
+  validateProjectExists,
+  TaskController.getTaskById
+)
+
 export default router
