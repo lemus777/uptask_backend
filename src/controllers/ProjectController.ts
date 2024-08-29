a001c93ac0decb250dad05ab3acb3d1b7b87c562
lemus777 <lemus777@gmail.com>
2024-08-29 13:55:02 +0200

    reading backend errors with useMutate

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -4,6 +4,7 @@ import Project from "../models/Project"
 export class ProjectController {
   static createProject = async (req : Request, res: Response) => {
     const project = new Project(req.body)
+
     try {
       await project.save()
       res.send('Proyecto creado correctamente')
