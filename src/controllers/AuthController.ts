e94534bd6618e25a3629ed8cb988b54201069db3
lemus777 <lemus777@gmail.com>
2025-01-03 19:22:35 +0100

    algorithm for create accounts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,16 @@
 import type { Request, Response } from "express"
+import User from '../models/User'
 
 export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
-    res.send('Desde /api/auth')
+    try {
+      const user = new User(req.body)
+      await user.save()
+
+      res.send('Cuenta creada, revisa tu email para confirmarla')
+    } catch (error) {
+      res.status(500).json({error: 'Hubo un error'})
+    }
   }
 }
