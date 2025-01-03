e4ec19b5f0e1bfd45299e299bed7f5f74d13f35b
lemus777 <lemus777@gmail.com>
2025-01-03 19:48:20 +0100

    avoiding duplicated users

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,7 +6,16 @@ export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body
+      const { password, email } = req.body
+
+      // Prevenir duplicados
+      const userExists = await User.findOne({email})
+      if(userExists) {
+        const error = new Error('El usuario ya está registrado')
+        return res.status(409).json({error: error.message})
+      }
+
+      // Crea un usuario
       const user = new User(req.body)
 
       // Hash password
