7dfa7d4e8b34df2b993bd9cc032f890951d5916f
lemus777 <lemus777@gmail.com>
2025-01-03 19:14:22 +0100

    creating controller and validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
+import { AuthController } from '../controllers/AuthController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Desde /api/auth')
-})
+router.post('/create-account',
+  body('name')
+    .notEmpty().withMessage('El nombre no puede ir vacío'),
+  body('password')
+    .isLength({min: 8}).withMessage('El password es muy corto, mínimo 8 caracteres'),
+  body('password_confirmation').custom((value, {req}) => {
+    if(value !== req.body.password) {
+      throw new Error('Los Password no son iguales')
+    }
+    return true
+  }),
+  body('email')
+    .isEmail().withMessage('E-mail no válido'),
+  handleInputErrors,
+  AuthController.createAccount
+)
 
 export default router
