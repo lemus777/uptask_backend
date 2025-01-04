f64c024ffe54c563b826037a9b3fdfb21b0d73e5
lemus777 <lemus777@gmail.com>
2025-01-04 12:55:49 +0100

    creating the token model

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express"
 import User from '../models/User'
-import hashPassword from '../utils/auth'
+import { hashPassword } from '../utils/auth'
 
 export class AuthController {
 
