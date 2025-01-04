29ece97a0160dfa01aaab1e3dba4651a788c8386
lemus777 <lemus777@gmail.com>
2025-01-04 16:22:40 +0100

    confirming account if the token is valid

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -51,6 +51,13 @@ export class AuthController {
         const error = new Error('Token no válido')
         return res.status(401).json({error: error.message})
       }
+
+      const user = await User.findById(tokenExists.user)
+      user.confirmed = true
+
+      await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+      res.send('Cuenta confirmada correctamente')
+      
     } catch (error) {
       res.status(500).json({error: 'Hubo un error'})
     }
