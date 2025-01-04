e61adab7bb2d75d274848b2efc9243a52db9c06c
lemus777 <lemus777@gmail.com>
2025-01-04 15:56:03 +0100

    sending to the new registered user an email with the token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,7 +3,7 @@ import User from '../models/User'
 import { hashPassword } from '../utils/auth'
 import Token from "../models/Token"
 import { generateToken } from "../utils/token"
-import { transporter } from "../config/nodemailer"
+import { AuthEmail } from "../emails/AuthEmail"
 
 export class AuthController {
 
@@ -30,12 +30,10 @@ export class AuthController {
       token.user = user.id
 
       // Enviar el email
-      await transporter.sendMail({
-        from: 'UpTask <admin@uptask.com>',
-        to: user.email,
-        subject: 'UpTask - Confirma tu cuenta',
-        text: 'Uptask - Confirma tu cuenta',
-        html: `<p>Probando e-mail</p>`
+      AuthEmail.sendConfirmationEmail({
+        email: user.email,
+        name: user.email,
+        token: token.token
       })
 
       await Promise.allSettled([user.save(), token.save()])
