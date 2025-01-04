eb379ff557267063a043fc1e57876ac9f2a6e6f6
lemus777 <lemus777@gmail.com>
2025-01-04 17:28:59 +0100

    checking the password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
@@ -84,7 +84,16 @@ export class AuthController {
         const error = new Error("La cuenta no ha sido confirmada, hemos enviado un e-mail de confirmación");
         return res.status(401).json({ error: error.message });
       }
-      console.log(user);
+      
+      // Revisar password
+      const isPasswordCorrect = await checkPassword(password, user.password)
+      if(!isPasswordCorrect) {
+        const error = new Error("Password incorrecto");
+        return res.status(401).json({ error: error.message });
+      }
+
+      res.send('Autenticado')
+
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
