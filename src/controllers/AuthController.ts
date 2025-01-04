d26d48dfdbf4f44d036e57e0250049e4429210c9
lemus777 <lemus777@gmail.com>
2025-01-04 13:54:43 +0100

    sending emails in Express

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,7 @@ import User from '../models/User'
 import { hashPassword } from '../utils/auth'
 import Token from "../models/Token"
 import { generateToken } from "../utils/token"
+import { transporter } from "../config/nodemailer"
 
 export class AuthController {
 
@@ -27,6 +28,15 @@ export class AuthController {
       const token = new Token()
       token.token = generateToken()
       token.user = user.id
+
+      // Enviar el email
+      await transporter.sendMail({
+        from: 'UpTask <admin@uptask.com>',
+        to: user.email,
+        subject: 'UpTask - Confirma tu cuenta',
+        text: 'Uptask - Confirma tu cuenta',
+        html: `<p>Probando e-mail</p>`
+      })
 
       await Promise.allSettled([user.save(), token.save()])
       res.send('Cuenta creada, revisa tu email para confirmarla')
