ef18ae2629b3b13e9672b2b2ec9184f68e2e19fc
lemus777 <lemus777@gmail.com>
2025-01-04 16:13:14 +0100

    validating the token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -42,4 +42,17 @@ export class AuthController {
       res.status(500).json({error: 'Hubo un error'})
     }
   }
+
+  static confirmAccount = async (req: Request, res: Response) => {
+    try {
+      const { token } = req.body
+      const tokenExists = await Token.findOne({token})
+      if(!tokenExists) {
+        const error = new Error('Token no válido')
+        return res.status(401).json({error: error.message})
+      }
+    } catch (error) {
+      res.status(500).json({error: 'Hubo un error'})
+    }
+  }
 }
