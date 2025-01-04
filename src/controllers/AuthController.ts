06606a57793424d3008f30a60920b1018ab23b84
lemus777 <lemus777@gmail.com>
2025-01-04 13:09:00 +0100

    generating a token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express"
 import User from '../models/User'
 import { hashPassword } from '../utils/auth'
+import Token from "../models/Token"
+import { generateToken } from "../utils/token"
 
 export class AuthController {
 
@@ -20,7 +22,13 @@ export class AuthController {
 
       // Hash password
       user.password = await hashPassword(password)
-      await user.save()
+
+      // Generar el token
+      const token = new Token()
+      token.token = generateToken()
+      token.user = user.id
+
+      await Promise.allSettled([user.save(), token.save()])
       res.send('Cuenta creada, revisa tu email para confirmarla')
     } catch (error) {
       res.status(500).json({error: 'Hubo un error'})
