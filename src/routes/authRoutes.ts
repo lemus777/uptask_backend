7c692f87aa15c7bd4cbd43855b75c6a389b6694e
lemus <lemus777@gmail.com>
2025-01-22 12:58:55 +0100

    requesting a new confirmation token

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -38,4 +38,11 @@ router.post('/login',
   AuthController.login
 )
 
+router.post('/request-code',
+  body('email')
+    .isEmail().withMessage('E-mail no válido'),
+  handleInputErrors,
+  AuthController.requestConfirmationCode
+)
+
 export default router
