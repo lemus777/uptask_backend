16ada52e61b046c7d1cb88c9c09739839260ed9d
lemus <lemus777@gmail.com>
2025-01-29 11:45:48 +0100

    validating if token for reset password is valid

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -52,4 +52,11 @@ router.post('/forgot-password',
   AuthController.forgotPassword
 )
 
+router.post('/validate-token',
+  body('token')
+    .notEmpty().withMessage('El Token no puede ir vacío'),
+  handleInputErrors,
+  AuthController.validateToken
+)
+
 export default router
