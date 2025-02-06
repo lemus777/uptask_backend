51c387466115214bcfa4a4a3925e95847b53797a
lemus <lemus777@gmail.com>
2025-02-06 11:59:38 +0100

    finishing the middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from "express"
 import jwt from "jsonwebtoken"
-import User from "../models/User"
+import User, { IUser } from "../models/User"
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser
+    }
+  }
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   const bearer = req.headers.authorization
@@ -14,8 +22,12 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
     if(typeof decoded === 'object' && decoded.id) {
-      const user = await User.findById(decoded.id)
-      console.log(user)
+      const user = await User.findById(decoded.id).select('_id name email')
+      if(user) {
+        req.user = user
+      } else {
+        res.status(500).json({error: 'Token no válido'})
+      }
     }
   } catch (error) {
     res.status(500).json({error: 'Token no válido'})
