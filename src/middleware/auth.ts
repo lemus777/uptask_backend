070c748ab666bb3e100eba0f515a58338fa10318
lemus <lemus777@gmail.com>
2025-02-06 11:49:42 +0100

    verificating the JWT in the backend

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import User from "../models/User"
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   const bearer = req.headers.authorization
@@ -7,6 +9,17 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
     return res.status(401).json({error: error.message})
   }
   const [, token] = bearer.split(' ')
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    if(typeof decoded === 'object' && decoded.id) {
+      const user = await User.findById(decoded.id)
+      console.log(user)
+    }
+  } catch (error) {
+    res.status(500).json({error: 'Token no válido'})
+  }
 
   next()
 }
