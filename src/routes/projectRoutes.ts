6f7ec98b5be6d72565a8fe9219274e9ea0dcd86c
lemus <lemus777@gmail.com>
2025-02-11 11:30:53 +0100

    protecting routes and projects if user have not access to them

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -9,8 +9,9 @@ import { authenticate } from "../middleware/auth";
 
 const router = Router()
 
+router.use(authenticate)
+
 router.post('/',
-  authenticate,
   body('projectName')
     .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
   body('clientName')
@@ -21,7 +22,7 @@ router.post('/',
   ProjectController.createProject
 )
 
-router.get('/', authenticate, ProjectController.getAllProjects)
+router.get('/', ProjectController.getAllProjects)
 
 router.get('/:id',
   param('id').isMongoId().withMessage('ID no válido'),
