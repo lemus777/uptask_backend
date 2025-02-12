f248f177c0e16659b6ce8a9ce08f7029b2ede0da
lemus <lemus777@gmail.com>
2025-02-12 12:38:33 +0100

    creating an endpoint and hook with the user's info

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -25,6 +25,7 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
       const user = await User.findById(decoded.id).select('_id name email')
       if(user) {
         req.user = user
+        next()
       } else {
         res.status(500).json({error: 'Token no válido'})
       }
@@ -32,6 +33,4 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
   } catch (error) {
     res.status(500).json({error: 'Token no válido'})
   }
-
-  next()
 }
