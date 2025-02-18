97064a45e24fe88cf6b82d78b8d4cf2383d7b76b
lemus <lemus777@gmail.com>
2025-02-18 12:07:29 +0100

    remove user from project fixed

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -44,14 +44,14 @@ export class TeamMembercontroller {
   }
 
   static removeMemberById = async (req: Request, res: Response) => {
-    const { id } = req.body
+    const { userId } = req.params
 
-    if(!req.project.team.some(team => team.toString() === id)) {
+    if(!req.project.team.some(team => team.toString() === userId)) {
       const error = new Error('El usuario no existe en el proyecto')
       return res.status(409).json({error: error.message})
     }
 
-    req.project.team = req.project.team.filter( teamMember => teamMember.toString() !== id)
+    req.project.team = req.project.team.filter( teamMember => teamMember.toString() !== userId)
     await req.project.save()
     res.send('Usuario eliminado correctamente')
   }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -116,8 +116,8 @@ router.post('/:projectId/team',
   TeamMembercontroller.addMemberById
 )
 
-router.delete('/:projectId/team',
-  body('id')
+router.delete('/:projectId/team/:userId',
+  param('userId')
     .isMongoId().withMessage('Id no válida'),
   handleInputErrors,
   TeamMembercontroller.removeMemberById
