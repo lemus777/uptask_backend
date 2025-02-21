38c514b40645f10e2df8a4e5a51b9de44ab5e5ab
lemus <lemus777@gmail.com>
2025-02-21 12:03:30 +0100

    modifying the task's model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -15,6 +15,7 @@ export interface ITask extends Document {
   description: string
   project: Types.ObjectId
   status: taskStatus
+  completedBy: Types.ObjectId
 }
 
 export const TaskSchema : Schema = new Schema({
@@ -36,6 +37,11 @@ export const TaskSchema : Schema = new Schema({
     type: String,
     enum: Object.values(taskStatus),
     default: taskStatus.PENDING
+  },
+  completedBy: {
+    type: Types.ObjectId,
+    ref: 'User',
+    defaul: null
   }
 }, {timestamps: true})
 
