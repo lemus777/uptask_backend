ead05b32dc941f2705445b653cb24df96091d15b
lemus <lemus777@gmail.com>
2025-02-27 11:35:19 +0100

    getting the notes from a task

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -135,4 +135,8 @@ router.post('/:projectId/tasks/:taskId/notes',
     NoteController.createNote
 )
 
+router.get('/:projectId/tasks/:taskId/notes',
+  NoteController.getTaskNotes
+)
+
 export default router
