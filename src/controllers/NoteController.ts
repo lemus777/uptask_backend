a344e25ab0dea76f08b04ac318c1426800e5118a
lemus <lemus777@gmail.com>
2025-02-27 11:56:25 +0100

    creating endpoint for delete notes

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from "express"
 import Note, { INote } from "../models/Note"
+import { Types } from "mongoose"
+
+type NoteParams = {
+  noteId: Types.ObjectId
+}
 
 export class NoteController {
   static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
@@ -23,6 +28,30 @@ export class NoteController {
     try {
       const notes = await Note.find({task: req.task.id})
       res.json(notes)
+    } catch (error) {
+      res.status(500).json({error: 'Hubo un error'})
+    }
+  }
+
+  static deleteNote = async (req: Request<NoteParams>, res: Response) => {
+    const { noteId } = req.params
+    const note = await Note.findById(noteId)
+
+    if(!note) {
+      const error = new Error('Nota no encontrada')
+      return res.status(404).json({error: error.message})
+    }
+
+    if(note.createdBy.toString() !== req.user.id.toString()) {
+      const error = new Error('Acción no válida')
+      return res.status(401).json({error: error.message})
+    }
+
+    req.task.notes = req.task.notes.filter(note => note.toString() !== noteId.toString())
+
+    try {
+      await Promise.allSettled([req.task.save(), note.deleteOne()])
+      res.send('Nota eliminada')
     } catch (error) {
       res.status(500).json({error: 'Hubo un error'})
     }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -139,4 +139,10 @@ router.get('/:projectId/tasks/:taskId/notes',
   NoteController.getTaskNotes
 )
 
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+  param('noteId').isMongoId().withMessage('Id no válida'),
+  handleInputErrors,
+  NoteController.deleteNote
+)
+
 export default router
