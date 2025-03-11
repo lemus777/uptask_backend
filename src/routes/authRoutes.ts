867b4dcc9e1b12e2fdc64f31ac86646b05b4c182
lemus <lemus777@gmail.com>
2025-03-11 10:38:01 +0100

    allowing the user to change the password

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -90,4 +90,21 @@ router.put('/profile',
   handleInputErrors,
   AuthController.updateProfile
 )
+
+router.post('/update/password',
+  authenticate,
+  body('current_password')
+    .notEmpty().withMessage('El password actual no puede ir vacío'),
+  body('password')
+    .isLength({min: 8}).withMessage('El password es muy corto, mínimo 8 caracteres'),
+  body('password_confirmation').custom((value, {req}) => {
+    if(value !== req.body.password) {
+      throw new Error('Los Password no son iguales')
+    }
+    return true
+  }),
+  handleInputErrors,
+  AuthController.updateCurrentUserPassword
+)
+
 export default router
